be3ed985b096c1cff23b589e19fbf037448787bc
Андрей Шалютов <shalyutov@outlook.com>
2022-11-22 00:44:57 +0500

    Добавлен класс описание программы обучения

---
+++ Univeris/Global/Degree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Univeris.Global
+{
+    /// <summary>
+    /// Направление обучения (программа обучения)
+    /// </summary>
+    internal class Degree
+    {
+        /// <summary>
+        /// Код/классификатор программы обучения
+        /// </summary>
+        public string Code { get; set; } = "";
+        /// <summary>
+        /// Наименование программы обучения
+        /// </summary>
+        public string Name { get; set; } = "";
+        /// <summary>
+        /// Выпускающая кафедра
+        /// </summary>
+        public Department Department { get; set; }
+        /// <summary>
+        /// Краткое описание
+        /// </summary>
+        public string Description { get; set; } = "";
+    }
+}
