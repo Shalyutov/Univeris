f1d0ec01d76d57ba1d7973e09e1e2e78992314a6
Андрей Шалютов <shalyutov@outlook.com>
2022-11-22 00:30:46 +0500

    Добавлен класс описывающий факультет

---
+++ Univeris/Global/Faculty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Univeris.Global
+{
+    internal class Faculty
+    {
+        /// <summary>
+        /// Наименование института/высшей школы/факультета
+        /// </summary>
+        public string Name { get; set; } = "";
+        /// <summary>
+        /// Краткое описание или ссылка на страницу (markdown)
+        /// </summary>
+        public string Description { get; set; } = "";
+    }
+}
